12d86a1ff1f582a53b677e265d67f852d449cb58
Oleksii Shnepov <Oleksii.Shnepov@pearson.com>
2022-04-10 00:35:11 +0300

    Moved date parsing to listing page

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 const yargs = require('yargs/yargs');
 const Crawler = require("crawler");
 
-const ITEM_LINK_SELECTOR = ".detailsLink";
+const LIST_ITEM_SELECTOR = ".offer"
+const LIST_ITEM_LINK_SELECTOR = ".detailsLink";
+const LIST_ITEM_PUBLICATION_DATE_SELECTOR = ".lheight16 span";
 const ITEM_BODY_SELECTOR = ".css-1wws9er";
-const ITEM_PUBLICATION_DATE_SELECTOR = ".css-19yf5ek";
 
 const argv = yargs(process.argv.slice(2))
     .option('baseUrl', {
@@ -84,31 +85,13 @@ function crawl(urls, convertResponse) {
     });
 }
 
-let PREVIOUSLY_FOUND_ITEMS = [];
-
-function collectItemUrls(baseUrl, numberOfPages) {
-    return crawl(buildPageUrls(baseUrl, numberOfPages), response => {
-        const itemUrls = []
-        const $ = response.$;
-        $(ITEM_LINK_SELECTOR).each((index, element) => {
-            itemUrls.push($(element).attr('href'));
-        });
-
-        return itemUrls;
-    }).then(foundItems => {
-        let results = foundItems.filter(foundItem => !PREVIOUSLY_FOUND_ITEMS.includes(foundItem));
-        PREVIOUSLY_FOUND_ITEMS = foundItems;
-        return results;
-    });
-}
-
 function parseDate(dateString) {
     function parseHours(dateString) {
-        return dateString.split(" ")[2].split(":")[0]
+        return dateString.split(" ")[1].split(":")[0]
     }
 
     function parseMinutes(dateString) {
-        return dateString.split(" ")[2].split(":")[1]
+        return dateString.split(" ")[1].split(":")[1]
     }
 
     function createCurrentDateWithParsedTime(dateString) {
@@ -120,42 +103,58 @@ function parseDate(dateString) {
         return date;
     }
 
-    function prepareStringForDateParsing(dateString) {
-        const months = [{str: "января", number: "01"},
-            {str: "февраля", number: "02"},
-            {str: "марта", number: "03"},
-            {str: "апреля", number: "04"},
-            {str: "мая", number: "05"},
-            {str: "июня", number: "06"},
-            {str: "июля", number: "07"},
-            {str: "августа", number: "08"},
-            {str: "сентября", number: "09"},
-            {str: "октября", number: "10"},
-            {str: "ноября", number: "11"},
-            {str: "декабря", number: "12"}];
+    function createYesterdayDateWithParsedTime(dateString) {
+        const date = createCurrentDateWithParsedTime(dateString);
+        date.setHours(date.getHours() - 24);
+        return date;
+    }
 
+    function parseDate(dateString) {
+        const months = [{str: "янв", number: 0},
+            {str: "фев", number: 1},
+            {str: "мар", number: 2},
+            {str: "апр", number: 3},
+            {str: "мая", number: 4},
+            {str: "июн", number: 5},
+            {str: "июл", number: 6},
+            {str: "авг", number: 7},
+            {str: "сен", number: 8},
+            {str: "окт", number: 9},
+            {str: "ноя", number: 10},
+            {str: "декя", number: 11}];
+
+        let date = null;
         months.forEach(month => {
             if (dateString.includes(month.str)) {
-                dateString = `${month.number}/` + dateString.replace(` ${month.str} `, "/");
+                date = new Date();
+                date.setMonth(month.number);
+                date.setDate(Number(dateString.split(" ")[0]))
+                date.setSeconds(0);
+                date.setMilliseconds(0);
             }
         })
 
-        return dateString.replace(" г.", "");
+        if(date == null) {
+            throw new Error(`There is no month mapping for string="${dateString}" in ${JSON.stringify(months)}`);
+        }
+
+        return date;
     }
 
     if (dateString.includes("Сегодня")) {
         return createCurrentDateWithParsedTime(dateString);
     }
 
-    return new Date(prepareStringForDateParsing((dateString)));
-}
-
-function filterItems(itemUrls, includePatters, excludePatterns) {
-    function isBodyMatched(text) {
-        return includePatters.some(pattern => text.includes(pattern))
-            && !excludePatterns.some(pattern => text.includes(pattern));
+    if (dateString.includes("Вчера")) {
+        return createYesterdayDateWithParsedTime(dateString);
     }
 
+    return new Date(parseDate((dateString)));
+}
+
+let PREVIOUSLY_FOUND_ITEMS = [];
+
+function collectItemUrls(baseUrl, numberOfPages) {
     function isPublicationDateMatched(publicationDate) {
         if (maxMinutes === -1) {
             return true;
@@ -165,16 +164,41 @@ function filterItems(itemUrls, includePatters, excludePatterns) {
         return publicationDate > currentDate.setMinutes(currentDate.getMinutes() - maxMinutes);
     }
 
-    return crawl(itemUrls, response => {
+    return crawl(buildPageUrls(baseUrl, numberOfPages), response => {
+        const itemUrls = []
+        const $ = response.$;
+
+        $(LIST_ITEM_SELECTOR).each((index, element) => {
+            const dateString = $($(element).find(LIST_ITEM_PUBLICATION_DATE_SELECTOR)[1]).text();
+            const publicationDate = parseDate(dateString)
+            if (isPublicationDateMatched(publicationDate)) {
+                itemUrls.push({
+                    "link": $(element).find(LIST_ITEM_LINK_SELECTOR).attr('href'),
+                    "date": publicationDate
+                });
+            }
+        });
+
+        return itemUrls;
+    }).then(foundItems => {
+        let results = foundItems.filter(foundItem => !PREVIOUSLY_FOUND_ITEMS.includes(foundItem));
+        PREVIOUSLY_FOUND_ITEMS = foundItems;
+        return results;
+    });
+}
+
+function filterItems(items, includePatters, excludePatterns) {
+    function isBodyMatched(text) {
+        return includePatters.some(pattern => text.includes(pattern))
+            && !excludePatterns.some(pattern => text.includes(pattern));
+    }
+
+    return crawl(items.map(item => item.link), response => {
         const $ = response.$;
 
         const body = $(ITEM_BODY_SELECTOR).text();
-        const publicationDate = parseDate($(ITEM_PUBLICATION_DATE_SELECTOR).text())
-        if (isBodyMatched(body) && isPublicationDateMatched(publicationDate)) {
-            return [{
-                "link": response.request.uri.href,
-                "date": publicationDate
-            }]
+        if (isBodyMatched(body)) {
+            return items.filter(item => item.link === response.request.uri.href);
         }
 
         return [];
@@ -188,11 +212,11 @@ function sortItemsByDate(items) {
 
 function run() {
     collectItemUrls(baseUrl, numberOfPages)
-        .then(itemUrls => filterItems(itemUrls, includes, excludes)
-            .then(filteredItemUrls => {
+        .then(items => filterItems(items, includes, excludes)
+            .then(filteredItem => {
                 console.log("--------------------------------------------------------------------------------")
-                if(filteredItemUrls.length > 0) {
-                    console.log("Found items:" + JSON.stringify(sortItemsByDate(filteredItemUrls), null, "  "));
+                if (filteredItem.length > 0) {
+                    console.log("Found items:" + JSON.stringify(sortItemsByDate(filteredItem), null, "  "));
                 } else {
                     console.log("No new items found");
                 }
