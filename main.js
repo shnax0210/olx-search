9ccf1b2db646d842a60a97819e2027e930f7b13a
Oleksii Shnepov <Oleksii.Shnepov@pearson.com>
2022-04-13 22:47:31 +0300

    Refactored

--- main.js
+++ main.js
@@ -48,31 +48,16 @@ const argv = yargs(process.argv.slice(2))
     .help()
     .alias('help', 'h').argv;
 
-const baseUrl = argv.baseUrl;
-const numberOfPages = argv.numberOfPages;
-const includes = argv.includes || [];
-const excludes = argv.excludes || [];
-const maxMinutes = argv.maxMinutes || -1;
-const loopSeconds = argv.loopSeconds || -1;
-const playSound = (typeof argv.playSound !== 'undefined') ? argv.playSound : (loopSeconds !== -1);
+const config = {};
+config.baseUrl = argv.baseUrl;
+config.numberOfPages = argv.numberOfPages || 1;
+config.includes = argv.includes || [];
+config.excludes = argv.excludes || [];
+config.maxMinutes = argv.maxMinutes || -1;
+config.loopSeconds = argv.loopSeconds || -1;
+config.playSound = (typeof argv.playSound !== 'undefined') ? argv.playSound : (config.loopSeconds !== -1);
 
-console.log(`Search parameters:
-    baseUrl=${baseUrl}
-    numberOfPages=${numberOfPages}
-    includes=${includes}
-    excludes=${excludes}
-    maxMinutes=${maxMinutes}
-    loopSeconds=${loopSeconds}
-    playSound=${playSound}`);
-
-function buildPageUrls(baseUrl, numberOfPages) {
-    const pageUrls = [];
-    for (let i = 1; i <= numberOfPages; i++) {
-        pageUrls.push(baseUrl + "&page=" + i)
-    }
-
-    return pageUrls;
-}
+console.log(`Search parameters: ${JSON.stringify(config, null, "  ")}`);
 
 function crawl(urls, convertResponse) {
     return new Promise((resolve, reject) => {
@@ -95,75 +80,84 @@ function crawl(urls, convertResponse) {
     });
 }
 
-function parseDate(dateString) {
-    function parseHours(dateString) {
-        return dateString.split(" ")[1].split(":")[0]
-    }
+function collectItems(baseUrl, numberOfPages) {
+    function buildPageUrls() {
+        const pageUrls = [];
+        for (let i = 1; i <= numberOfPages; i++) {
+            pageUrls.push(baseUrl + "&page=" + i)
+        }
 
-    function parseMinutes(dateString) {
-        return dateString.split(" ")[1].split(":")[1]
-    }
-
-    function createCurrentDateWithParsedTime(dateString) {
-        const date = new Date();
-        date.setHours(parseHours(dateString));
-        date.setMinutes(parseMinutes(dateString));
-        date.setSeconds(0);
-        date.setMilliseconds(0);
-        return date;
-    }
-
-    function createYesterdayDateWithParsedTime(dateString) {
-        const date = createCurrentDateWithParsedTime(dateString);
-        date.setHours(date.getHours() - 24);
-        return date;
+        return pageUrls;
     }
 
     function parseDate(dateString) {
-        const months = [{str: "янв", number: 0},
-            {str: "фев", number: 1},
-            {str: "мар", number: 2},
-            {str: "апр", number: 3},
-            {str: "мая", number: 4},
-            {str: "июн", number: 5},
-            {str: "июл", number: 6},
-            {str: "авг", number: 7},
-            {str: "сен", number: 8},
-            {str: "окт", number: 9},
-            {str: "ноя", number: 10},
-            {str: "дек", number: 11}];
-
-        let date = null;
-        months.forEach(month => {
-            if (dateString.includes(month.str)) {
-                date = new Date();
-                date.setMonth(month.number);
-                date.setDate(Number(dateString.split(" ")[0]))
-                date.setSeconds(0);
-                date.setMilliseconds(0);
-            }
-        })
-
-        if(date == null) {
-            throw new Error(`There is no month mapping for string="${dateString}" in ${JSON.stringify(months)}`);
+        function parseHours(dateString) {
+            return dateString.split(" ")[1].split(":")[0]
         }
 
-        return date;
+        function parseMinutes(dateString) {
+            return dateString.split(" ")[1].split(":")[1]
+        }
+
+        function createCurrentDateWithParsedTime(dateString) {
+            const date = new Date();
+            date.setHours(parseHours(dateString));
+            date.setMinutes(parseMinutes(dateString));
+            date.setSeconds(0);
+            date.setMilliseconds(0);
+            return date;
+        }
+
+        function createYesterdayDateWithParsedTime(dateString) {
+            const date = createCurrentDateWithParsedTime(dateString);
+            date.setHours(date.getHours() - 24);
+            return date;
+        }
+
+        function parseDate(dateString) {
+            const months = [{str: "янв", number: 0},
+                {str: "фев", number: 1},
+                {str: "мар", number: 2},
+                {str: "апр", number: 3},
+                {str: "мая", number: 4},
+                {str: "июн", number: 5},
+                {str: "июл", number: 6},
+                {str: "авг", number: 7},
+                {str: "сен", number: 8},
+                {str: "окт", number: 9},
+                {str: "ноя", number: 10},
+                {str: "дек", number: 11}];
+
+            let date = null;
+            months.forEach(month => {
+                if (dateString.includes(month.str)) {
+                    date = new Date();
+                    date.setMonth(month.number);
+                    date.setDate(Number(dateString.split(" ")[0]))
+                    date.setSeconds(0);
+                    date.setMilliseconds(0);
+                }
+            })
+
+            if(date == null) {
+                throw new Error(`There is no month mapping for string="${dateString}" in ${JSON.stringify(months)}`);
+            }
+
+            return date;
+        }
+
+        if (dateString.includes("Сегодня")) {
+            return createCurrentDateWithParsedTime(dateString);
+        }
+
+        if (dateString.includes("Вчера")) {
+            return createYesterdayDateWithParsedTime(dateString);
+        }
+
+        return new Date(parseDate((dateString)));
     }
 
-    if (dateString.includes("Сегодня")) {
-        return createCurrentDateWithParsedTime(dateString);
-    }
-
-    if (dateString.includes("Вчера")) {
-        return createYesterdayDateWithParsedTime(dateString);
-    }
-
-    return new Date(parseDate((dateString)));
-}
-
-function collectItems(baseUrl, numberOfPages) {
-    return crawl(buildPageUrls(baseUrl, numberOfPages), response => {
+    return crawl(buildPageUrls(), response => {
         const items = []
         const $ = response.$;
 
@@ -180,7 +174,7 @@ function collectItems(baseUrl, numberOfPages) {
     });
 }
 
-function filterItems(items, includePatters, excludePatterns) {
+function filterItems(items, maxMinutes, includes, excludes) {
     function filterItemsByPublicationDate(items) {
         function isPublicationDateMatched(publicationDate) {
             if (maxMinutes === -1) {
@@ -198,15 +192,15 @@ function filterItems(items, includePatters, excludePatterns) {
         return items.filter(item => !ALREADY_WATCHED_ITEM_LINKS.has(item.link));
     }
 
-    function filterItemsByBodyContent(items, includePatters, excludePatterns) {
-        if ((includePatters.length === 0 && excludePatterns.length === 0) || items.length === 0) {
+    function filterItemsByBodyContent(items) {
+        if ((includes.length === 0 && excludes.length === 0) || items.length === 0) {
             items.forEach(item => ALREADY_WATCHED_ITEM_LINKS.add(item.link));
             return new Promise((resolve, reject) => resolve(items));
         }
 
         function isBodyMatched(text) {
-            return includePatters.some(pattern => text.includes(pattern))
-                && !excludePatterns.some(pattern => text.includes(pattern));
+            return includes.some(pattern => text.includes(pattern))
+                && !excludes.some(pattern => text.includes(pattern));
         }
 
         return crawl(items.map(item => item.link), response => {
@@ -225,15 +219,14 @@ function filterItems(items, includePatters, excludePatterns) {
     items = filterItemsByPublicationDate(items);
     items = filterOnlyNewItems(items);
 
-    return filterItemsByBodyContent(items, includePatters, excludePatterns);
+    return filterItemsByBodyContent(items);
 }
 
-function sortItemsByDate(items) {
-    return items.sort((a, b) => (a.date > b.date) ? -1 : 1);
-}
+function processResults(resultItems, playSound) {
+    function sortItemsByDate(items) {
+        return items.sort((a, b) => (a.date > b.date) ? -1 : 1);
+    }
 
-
-function processResults(resultItems) {
     console.log("--------------------------------------------------------------------------------")
     if (resultItems.length > 0) {
         console.log("Found items:" + JSON.stringify(sortItemsByDate(resultItems), null, "  "));
@@ -246,19 +239,19 @@ function processResults(resultItems) {
     console.log("--------------------------------------------------------------------------------")
 }
 
-function run() {
-    collectItems(baseUrl, numberOfPages)
-        .then(items => filterItems(items, includes, excludes)
-            .then(filteredItem => processResults(filteredItem)));
+function run(config) {
+    collectItems(config.baseUrl, config.numberOfPages)
+        .then(items => filterItems(items, config.maxMinutes, config.includes, config.excludes)
+            .then(items => processResults(items, config.playSound)));
 }
 
-function runInLoop() {
-    run();
-    setTimeout(runInLoop, loopSeconds * 1000)
+function runInLoop(config) {
+    run(config);
+    setTimeout(runInLoop, config.loopSeconds * 1000, config)
 }
 
-if (loopSeconds === -1) {
-    run();
+if (config.loopSeconds === -1) {
+    run(config);
 } else {
-    runInLoop();
+    runInLoop(config);
 }
